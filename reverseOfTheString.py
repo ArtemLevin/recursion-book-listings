7b7a573f864f52411c2ee9868d0f4053f5838da9
ArtemLevin <python_study_2022@mail.ru>
2023-07-31 01:06:42 +0300

    reverse of the string listing

---
+++ reverseOfTheString.py
@@ -0,0 +1,9 @@
+def rev(theString):
+    if len(theString) == 0 or len(theString) == 1:
+        return theString
+    else:
+        head = theString[0]
+        tail = theString[1:]
+        return rev(tail) + head
+
+print(rev('hello world'))
